0d477831f2e7ce6460cbfa2db74854e4e4e65fbc
GithubApres <125526105+GithubApres@users.noreply.github.com>
2023-06-08 22:07:09 -0400

    candlestick graph - anychart.onDocumentReady

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //var data = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&apikey={APIKEY}&outputsize=full').json()"
 //var chart = anychart.fromJson(data);
 
-anychart.onDocumentReady(function () {
+anychart.onDocumentReady(function (ticker) {
   //anychart.data.loadCsvFile(
    // 'https://gist.githubusercontent.com/shacheeswadia/cd509e0b0c03964ca86ae7d894137043/raw/5f336c644ad61728dbac93026f3268b86b8d0680/teslaDailyData.csv',
     //'Tesla1.csv',
@@ -9,58 +9,54 @@ anychart.onDocumentReady(function () {
       // create data table on loaded data
     anychart.data.loadJsonFile("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo",  
     function (data) {
-      var dataTable = anychart.data.table();
-      dataTable.addData(data);
-      console.log(data);
-      console.log("................");
-      console.log(dataTable);
 
       let myStockData = data['Time Series (5min)'];
       //console.log(myStockData);
-      let dates = Object.keys(myStockData)
+      let datesRaw = Object.keys(myStockData)
+      let dates = []
       let open = []
       let close = []
       let low = []
       let high = []
+      let volume = []
 
-      for (let i = 0; i < dates.length; i++) {
-          // console.log(Object.keys(myStockData[dates[i]]));
-          open.push(myStockData[dates[i]]['1. open'])
-          high.push(myStockData[dates[i]]['2. high'])
-          low.push(myStockData[dates[i]]['3. low'])
-          close.push(myStockData[dates[i]]['4. close'])
+      // for (let i = 0; i < dates.length; i++) {
+      //     // console.log(Object.keys(myStockData[dates[i]]));
+      //     open.push(myStockData[dates[i]]['1. open'])
+      //     high.push(myStockData[dates[i]]['2. high'])
+      //     low.push(myStockData[dates[i]]['3. low'])
+      //     close.push(myStockData[dates[i]]['4. close'])
                    
-      }
+      // }          
+      
+      let dataString = ''
 
-      console.log(dates);
-      console.log(";;;;;;;dates;;;;;;;;;;;;")
-      console.log(open);
-      console.log(high);
-      console.log(low);
-      console.log(close);
-     
+      for (let i = 0; i < datesRaw.length; i++) { 
+        dates.push(datesRaw[i])//.split(' ')[1])
+        open.push(myStockData[datesRaw[i]]['1. open'])
+        high.push(myStockData[datesRaw[i]]['2. high'])
+        low.push(myStockData[datesRaw[i]]['3. low'])
+        close.push(myStockData[datesRaw[i]]['4. close'])
+        volume.push(myStockData[datesRaw[i]]['5. volume'])
 
+        let stockString = `${dates[i]},${open[i]},${high[i]},${low[i]},${close[i]},${volume[i]}\n`
+        dataString += stockString
+        // volume.push(myStockData[dates[i]]['5. volume'])
+                 
+      };
 
-      // console.log("................")
-      // console.log(dataTable);
+      console.log(dataString);
 
-      // map loaded data for the candlestick series
-      // var mapping = dataTable.mapAs({
-      //   open: 1,
-      //   high: 2,
-      //   low: 3,
-      //   close: 4
-      // });
+      var dataTable = anychart.data.table();
+      dataTable.addData(dataString);
 
       var mapping = dataTable.mapAs({
         open: 1,
         high: 2,
         low: 3,
-        close: 4
+        close: 4,
+        volume: 5
       });
-
-      //console.log("................");
-      console.log(mapping);
 
       // create stock chart
       var chart = anychart.stock();
@@ -72,7 +68,7 @@ anychart.onDocumentReady(function () {
       plot.yGrid(true).xGrid(true).yMinorGrid(true).xMinorGrid(true);
 
       var series = plot.candlestick(mapping)
-        .name('Tesla');
+        .name('IBM');
 
       series.legendItem().iconType('rising-falling');
 
@@ -80,29 +76,29 @@ anychart.onDocumentReady(function () {
       chart.scroller().candlestick(mapping);
 
       // set chart selected date/time range
-      chart.selectRange('2020-11-27', '2021-11-26');
+      //chart.selectRange('19:30:00', '19:15:00');
 
       // create range picker
       var rangePicker = anychart.ui.rangePicker();
+      rangePicker.format('dd-MM-yyyy HH:mm:ss')
 
       // init range picker
       rangePicker.render(chart);
 
       // create range selector
-      var rangeSelector = anychart.ui.rangeSelector();
+      //var rangeSelector = anychart.ui.rangeSelector();
 
       // init range selector
-      rangeSelector.render(chart);
+      //rangeSelector.render(chart);
 
       // sets the title of the chart
-      chart.title('Tesla Inc. Stock Chart');
+      chart.title('IBM Stock Chart');
 
       // set container id for the chart
       chart.container('container');
 
       // initiate chart drawing
       chart.draw();
-
     }
   );
 });
