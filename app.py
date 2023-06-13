d867cfa2a2ad4fe9986146e656ca97f1e94388f8
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-06-13 09:42:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
-# Import the dependencies.
-# import datetime as dt
-import numpy as np
-
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
 
-
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///Resources/StockStats.sqlite")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(autoload_with=engine)
-
-# Save references to each table
-Ticker = Base.classes.ticker
-TimeStamp = Base.classes.timestamp
-Open = Base.classes.open
-High = Base.classes.high
-Low = Base.classes.low
-Close = Base.classes.close
-
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/StockData"
+mongo = PyMongo(app)
 
+@app.route('/stockinfo', methods=['GET'])
+def get_stockinfo():
+    data = mongo.db.StockInfo.find()
+    return dumps(data)  # Converts BSON to JSON
 
-#################################################
-# Flask Routes
-#################################################
+@app.route('/openclose', methods=['GET'])
+def get_openclose():
+    data = mongo.db.OpenClose.find()
+    return dumps(data)  # Converts BSON to JSON
 
+@app.route('/highlow', methods=['GET'])
+def get_highlow():
+    data = mongo.db.HighLow.find()
+    return dumps(data)  # Converts BSON to JSON
 
-
+@app.route('/volume', methods=['GET'])
+def get_volume():
+    data = mongo.db.Volume.find()
+    return dumps(data)  # Converts BSON to JSON
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
