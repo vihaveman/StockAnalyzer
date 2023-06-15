98c4c1c051fe6af87fead85be22df2eab15c9123
Violetta Haveman <120049344+vihaveman@users.noreply.github.com>
2023-06-15 17:20:47 -0400

    Update bargraph.js

--- static/js/bargraph.js
+++ static/js/bargraph.js
@@ -1,17 +1,11 @@
-// Define the tickerDropdown variable
-const tickerDropdown = document.getElementById("tickerDropdown");
-
-// Fetch data and set up event listener
-google.charts.load('current', { packages: ['corechart'] });
-google.charts.setOnLoadCallback(fetchData);
-
 function fetchData() {
   const url = '/volume';
 
-  fetch(url)
+  return fetch(url)
     .then(response => response.json())
     .then(data => {
       const tickers = [...new Set(data.map(item => item.Ticker))];
+      const tickerDropdown = document.getElementById("tickerDropdown");
 
       tickers.forEach(ticker => {
         const option = document.createElement("option");
@@ -20,48 +14,82 @@ function fetchData() {
       });
 
       // When a dropdown value is changed, update the visualizations.
-      tickerDropdown.addEventListener("change", () => updateVisualizations(data));
-      
-      d3.select("#volumeChart").html("");
+      tickerDropdown.addEventListener("change", updateVisualizations);
+      updateVisualizations();
 
-      // Call updateVisualizations with the fetched data
-      updateVisualizations(data);
+      function updateVisualizations() {
+        const selectedTicker = tickerDropdown.value;
+        const filteredData = data.filter(item => item.Ticker === selectedTicker);
+        const processedData = convertData(filteredData);
+        drawBarGraph(processedData);
+      }
     })
     .catch(error => console.log(error));
 }
 
-function updateVisualizations(data) {
-  const selectedTicker = tickerDropdown.value;
-  const filteredData = data.filter(item => item.Ticker === selectedTicker);
-  const processedData = filteredData.map(item => ({
-    date: new Date(item['Date']).toLocaleDateString(),
-    volume: parseFloat(item['Volume'])
+function convertData(data) {
+  return data.map(item => ({
+    ...item,
+    Date: new Date(item.Date),
+    Volume: parseFloat(item.Volume)
   }));
-  drawBarGraph(processedData);
 }
 
 function drawBarGraph(data) {
-  const dataTable = new google.visualization.DataTable();
-  dataTable.addColumn('string', 'Date');
-  dataTable.addColumn('number', 'Volume');
+  const dates = data.map(d => d.Date);
+  const volumes = data.map(d => d.Volume);
 
-  data.forEach(item => {
-    dataTable.addRow([item.date, item.volume]);
-  });
+  const bars = d3
+    .select('#barChartContainer')
+    .selectAll('.bar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('class', d => (d.Volume >= 100 ? 'bar green-bar' : 'bar red-bar'))
+    .attr('x', (d, i) => i * 20)
+    .attr('y', d => (d.Volume >= 100 ? d.Volume : 0))
+    .attr('width', 10)
+    .attr('height', d => Math.abs(d.Volume));
 
-  const options = {
-    title: 'Volume by Date',
-    hAxis: {
-      title: 'Date'
-    },
-    vAxis: {
-      title: 'Volume ($M)'
+  const trace = {
+    x: dates,
+    y: volumes,
+    type: 'bar',
+    marker: {
+      color: data.map(d => (d.Volume >= 100 ? 'green' : 'red'))
     }
   };
 
-  const chart = new google.visualization.ColumnChart(document.getElementById('chart-container'));
-  chart.draw(dataTable, options);
+  const layout = {
+    title: 'Trading Volume',
+    xaxis: {
+      title: 'Date'
+    },
+    yaxis: {
+      title: 'Volume (M)'
+    },
+    legend: {
+      x: 0,
+      y: 1,
+      traceorder: 'normal',
+      bgcolor: 'rgba(0,0,0,0)',
+      bordercolor: 'rgba(0,0,0,0)',
+      borderwidth: 0,
+      orientation: 'h',
+      itemsizing: 'constant',
+      itemwidth: 80,
+      itemheight: 20,
+      itemclick: false,
+      xanchor: 'left'
+    }
+  };
+
+  const dataPlotly = [trace];
+
+  Plotly.newPlot('barChartContainer', dataPlotly, layout);
 }
+
+fetchData();
 
 
 
